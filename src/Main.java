ccdb41373777c2ffc9095506554618c558e97a31
poeFinalCount <st10441569@rcconnect.edu.za>
2024-05-23 17:20:28 +0200

    part 1 and part2(its not looking good)

--- src/Main.java
+++ src/Main.java
@@ -1,40 +1,40 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 import javax.swing.JOptionPane;
 
 public class Main {
 
     static Map<String, String> users = new HashMap<String, String>();
-    static Map<String, ArrayList<Task>> userTasks = new HashMap<>();
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int choice=0;
-        while(choice!=3) {
+        int choice = 0;
+        while (choice != 3) {
             String input = JOptionPane.showInputDialog(null, "1. Registration\n2. Login\n3. Quit");
             choice = Integer.parseInt(input);
-            switch(choice){
-                case 1: registerUser(sc);
+            switch (choice) {
+                case 1:
+                    registerUser(sc);
                     break;
-                case 2: loginUser(sc);
+                case 2:
+                    loginUser(sc);
                     break;
                 case 3:
                     break;
                 default:
-                    System.out.println("Invalid Choice");
+                    System.out.println("Invalid Choice.");
             }
         }
         sc.close();
     }
-    // user input for regestration
+
+    // user input for registration
     static void registerUser(Scanner sc) {
-        String user = JOptionPane.showInputDialog("create a username");
-        String password = JOptionPane.showInputDialog("create a password");
-        String name = JOptionPane.showInputDialog("Enter name");
-        String surname = JOptionPane.showInputDialog("Enter surname");
+        String user = JOptionPane.showInputDialog("create a username: ");
+        String password = JOptionPane.showInputDialog("create a password: ");
+        String name = JOptionPane.showInputDialog("Enter name:");
+        String surname = JOptionPane.showInputDialog("Enter surname: ");
         if (users.containsKey(user)) {
-            System.out.println("User already exists");
+            System.out.println("User already exists.");
         } else {
             users.put(user, password);
             System.out.println("Registration Successful.");
@@ -48,19 +48,20 @@ public class Main {
         if (checkPassword(password)) {
             JOptionPane.showMessageDialog(null, "password successfully captured");
         } else {
-            JOptionPane.showMessageDialog(null, "password is not correctly formatted, please ensure that the password conatains at least 8 characters, a capital letter, a number and a special character");
+            JOptionPane.showMessageDialog(null, "password is not correctly formatted, please ensure that the password contains at least 8 characters, a capital letter, a number and a special character");
         }
     }
 
-    static void loginUser(Scanner sc){
+    static void loginUser(Scanner sc) {
         String user = JOptionPane.showInputDialog("Enter your username: ");
         String password = JOptionPane.showInputDialog("Enter your password: ");
         if (!users.containsKey(user)) {
-            System.out.println("Invalid username or password");
+            System.out.println("Invalid username or password.");
         } else {
             if (users.get(user).equals(password)) {
                 System.out.println("Login Successful.");
-                JOptionPane.showMessageDialog(null, "Welcome " + user + ", it is great to see you again");
+                JOptionPane.showMessageDialog(null, "Welcome to easy kanban!!");
+                JOptionPane.showMessageDialog(null, "Welcome " + user + " , it is great to see you again.");
                 int choice = 0;
                 while (choice != 3) {
                     String input = JOptionPane.showInputDialog(null, "1. Add Task\n2. Show report\n3. Quit");
@@ -75,11 +76,12 @@ public class Main {
                         case 3:
                             break;
                         default:
-                            System.out.println("Invalid choice");
+                            System.out.println("Invalid choice.");
+
                     }
                 }
             } else {
-                System.out.println("Username or password incorrect, please try again");
+                System.out.println("Username or password incorrect, please try again.");
             }
         }
     }
@@ -103,38 +105,55 @@ public class Main {
         }
         return false;
     }
+
     static void addTask(String user) {
-        String taskDescription = JOptionPane.showInputDialog("Enter the task Description: ");
+        String taskName = JOptionPane.showInputDialog("Enter task name ");
+        String taskDescription;
+        do {
+            taskDescription = JOptionPane.showInputDialog("Enter the task Description: ");
+            if (taskDescription.length() > 50) {
+                JOptionPane.showMessageDialog(null, "Please enter a Task Description of less than 50 characters");
+            }
+        } while (taskDescription.length() > 50);
+
+        JOptionPane.showMessageDialog(null, "Task successfully captured");
+
+        //entering basic information
         String firstName = JOptionPane.showInputDialog("Enter developer name: ");
         String sSurname = JOptionPane.showInputDialog("Enter developer surname: ");
-        String[] statuses = {"To do", "Doing", "Done"};
-        String status = (String) JOptionPane.showInputDialog(null, "Select task status");
-        ArrayList<Task> tasks = userTasks.get(user);
-        tasks.add(new Task(taskDescription, firstName, sSurname, status));
-        System.out.println("Task added");
-        }
-    }
-        static void showReport() {
-            System.out.println("Tasks for " + user + ": ");
-            for (int i = 0; i < tasks.size(); i++) {
-                Task task = tasks.get(i);
-                System.out.println((i + 1) + ". Description: " + task.getDescription() + ", Developer: " + task.getFirstName() + " " + task.getSSurname() + ", Status: " + task.getStatus());
-    }
-}
-class Task {
-private String Description;
-private String firstName;
-private String sSurname;
-private String status;
+        // this is an array that allows you to choose between the 3
+        Scanner scanner = new Scanner(System.in);
 
-    public Task(String description, String firstName, String lastName, String status) {
-        this.Description = description;
-        this.firstName = firstName;
-        this.sSurname = lastName;
-        this.status = status;
+        String[] statuses = {"To do", "Doing", "Done"};
+        statuses[0] = "To do";
+        statuses[1] = "Doing";
+        statuses[2] = "Done";
+        System.out.println(Arrays.toString(statuses));
+        System.out.println("Task added");
+        // Display a dialog box with the statuses array as options
+        int statusChoice = JOptionPane.showOptionDialog(null, "Select task status:", "Task Status", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, statuses, statuses[0]);
+
+        // Check if the user made a selection
+        if (statusChoice != JOptionPane.CLOSED_OPTION) {
+            String selectedStatus = statuses[statusChoice];
+            System.out.println("Task added with status: " + selectedStatus);
+            // Here you can further process the task with the selected status
+        } else {
+            System.out.println("Task addition cancelled.");
+        }
+        StringBuilder displayMenu = new StringBuilder();
+        displayMenu.append("Task name: ").append(taskName);
+        displayMenu.append("Description: ").append(taskDescription);
+        displayMenu.append("Developer name & Surname").append(firstName).append(sSurname);
+        displayMenu.append("Status: ").append(statusChoice);
+
+        JOptionPane.showMessageDialog(null, displayMenu.toString(), "Task Information", JOptionPane.INFORMATION_MESSAGE);
+
     }
-}
-public String getDecription() {
-return description;
+
+
+    static void showReport() {
+    JOptionPane.showMessageDialog(null,"Coming soon.");
+    }
 }
 
